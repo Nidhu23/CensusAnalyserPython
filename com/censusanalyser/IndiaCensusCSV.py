f9f6c7b29cd105209a6ab51ce891fe4abb28c1fb
“Nidhu23” <nidhunanik@gmail.com>
2020-08-26 23:05:22 +0530

    [Nidhuna]Add: Class for StateCodeCSV DTO

--- com/censusanalyser/IndiaCensusCSV.py
+++ com/censusanalyser/IndiaCensusCSV.py
@@ -7,3 +7,12 @@ class IndiaCensusCSV:
 
     def __repr__(self):
         return self.state + "," + self.population + "," + self.density + "," + self.area
+
+
+class StateCodeCSV:
+    def __init__(self):
+        self.state = "State Name"
+        self.state_code = "StateCode"
+
+    def __repr__(self):
+        return self.state + "," + self.state_code
