9cc49b88abf3b1ff80fb73df16805c03769ca64c
“Nidhu23” <nidhunanik@gmail.com>
2020-08-26 23:04:47 +0530

    [Nidhuna]Add: Method that loads state code data and returns row count

--- com/censusanalyser/CensusAnalyser.py
+++ com/censusanalyser/CensusAnalyser.py
@@ -1,17 +1,27 @@
 import pandas as pd
 from com.censusanalyser.CensusAnalyserError import CensusAnalyserError
-from com.censusanalyser.IndiaCensusCSV import IndiaCensusCSV
+from com.censusanalyser.IndiaCensusCSV import IndiaCensusCSV, StateCodeCSV
 
 
 class CsvLoader:
     def __init__(self, path):
         self.path = path
 
-    def record_counter(self):
+    def load_census_data(self):
         try:
             census_col_list = repr(IndiaCensusCSV()).split(",")
             census_list = pd.read_csv(self.path, usecols=census_col_list)
             return len(census_list)
+        except FileNotFoundError:
+            raise CensusAnalyserError("Check file path")
+        except ValueError:
+            raise CensusAnalyserError("Wrong delimiter Or Headers do not match")
+
+    def load_state_data(self):
+        try:
+            state_col_list = repr(StateCodeCSV()).split(",")
+            state_list = pd.read_csv(self.path, usecols=state_col_list)
+            return len(state_list)
         except FileNotFoundError:
             raise CensusAnalyserError("Check file path")
         except ValueError:
