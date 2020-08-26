c42035d53babfb4ce3eeff08ce94052d579aa3a0
“Nidhu23” <nidhunanik@gmail.com>
2020-08-27 02:05:33 +0530

    [Nidhuna]Add: Method that sorts by area

--- com/censusanalyser/CensusAnalyser.py
+++ com/censusanalyser/CensusAnalyser.py
@@ -30,3 +30,7 @@ class CensusAnalyser:
     def sort_by_density(self):
         CensusAnalyser.__data_list.sort_values(by=[IndiaCensusCSV().density], inplace=True, ascending=False)
         return CensusAnalyser.__data_list.to_json(orient="records")
+
+    def sort_by_area(self):
+        CensusAnalyser.__data_list.sort_values(by=[IndiaCensusCSV().area], inplace=True, ascending=False)
+        return CensusAnalyser.__data_list.to_json(orient="records")
