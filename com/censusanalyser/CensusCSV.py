af9b58eb129093b60eca3b5966bc2274e1fdb0df
“Nidhu23” <nidhunanik@gmail.com>
2020-08-27 22:19:54 +0530

    [Nidhuna]Add: Class to map Us census data csv headers

--- com/censusanalyser/CensusCSV.py
+++ com/censusanalyser/CensusCSV.py
@@ -16,3 +16,15 @@ class StateCodeCSV:
 
     def __repr__(self):
         return self.state + "," + self.state_code
+
+
+class USCensusCSV:
+    def __init__(self):
+        self.state = "State"
+        self.state_code = "State Id"
+        self.area = "Total area"
+        self.population = "Population"
+        self.density = "Population Density"
+
+    def __repr__(self):
+        return self.state + "," + self.population + "," + self.density + "," + self.area + "," + self.state_code
