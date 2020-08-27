0e7c7f5fe6effa2fb1dc4ee4b0ca734ea949ba06
“Nidhu23” <nidhunanik@gmail.com>
2020-08-27 21:58:03 +0530

    [Nidhuna]Add: Test Case for method that loads us census csv data and returns count

--- com.censusanalyser.testing/test_CensusAnalyser.py
+++ com.censusanalyser.testing/test_CensusAnalyser.py
@@ -13,35 +13,36 @@ STATE_CODE_CSV_FILE_PATH = r"resources\IndiaStateCode.csv"
 STATE_CODE_CSV_FILE_WRONG_PATH = r"com.censusanalyser.testingresources\IndiaStateCode.csv"
 STATE_CODE_CSV_FILE_WRONG_TYPE = r"resources\IndiaStateCode.gif"
 STATE_CODE_CSV_FILE_WRONG_DELIMITER = r"resources\IndiaStateCodeWrongDelimiter.csv"
+US_CENSUS_CSV_FILE_PATH = r"resources\USCensusData.csv"
 
 
 def test_givenIndiaCensusCSVFile_WhenCounted_ShouldReturnRecordsCount():
     census_analyser = CensusAnalyser()
-    assert census_analyser.census_record_counter(CENSUS_CSV_FILE_PATH) == 29
+    assert census_analyser.india_census_record_counter(CENSUS_CSV_FILE_PATH) == 29
 
 
 def test_givenIndiaCensusCSVFile_WhenWrongPath_ShouldRaiseException():
     census_analyser = CensusAnalyser()
     with pytest.raises(CensusAnalyserError):
-        assert census_analyser.census_record_counter(CENSUS_CSV_FILE_WRONG_PATH)
+        assert census_analyser.india_census_record_counter(CENSUS_CSV_FILE_WRONG_PATH)
 
 
 def test_givenIndiaCensusCSVFile_WhenWrongFileType_ShouldRaiseException():
     census_analyser = CensusAnalyser()
     with pytest.raises(CensusAnalyserError):
-        assert census_analyser.census_record_counter(CENSUS_CSV_FILE_WRONG_TYPE)
+        assert census_analyser.india_census_record_counter(CENSUS_CSV_FILE_WRONG_TYPE)
 
 
 def test_givenIndiaCensusCSVFile_WhenDelimiterWrong_ShouldRaiseException():
     census_analyser = CensusAnalyser()
     with pytest.raises(CensusAnalyserError):
-        assert census_analyser.census_record_counter(CENSUS_CSV_FILE_WRONG_DELIMITER)
+        assert census_analyser.india_census_record_counter(CENSUS_CSV_FILE_WRONG_DELIMITER)
 
 
 def test_givenIndiaCensusCSVFile_WhenHeadersWrong_ShouldRaiseException():
     census_analyser = CensusAnalyser()
     with pytest.raises(CensusAnalyserError):
-        assert census_analyser.census_record_counter(STATE_CODE_CSV_FILE_PATH)
+        assert census_analyser.india_census_record_counter(STATE_CODE_CSV_FILE_PATH)
 
 
 def test_givenStateCodeCSVFile_WhenLoaded_ShouldReturnRecordsCount():
@@ -50,7 +51,7 @@ def test_givenStateCodeCSVFile_WhenLoaded_ShouldReturnRecordsCount():
 
 def test_givenCensusCSVFile_WhenSortedByState_ShouldReturnSortedResult():
     census_analyser = CensusAnalyser()
-    census_analyser.census_record_counter(CENSUS_CSV_FILE_PATH)
+    census_analyser.india_census_record_counter(CENSUS_CSV_FILE_PATH)
     sorted_json = census_analyser.sort_by_state()
     json_dict = json.loads(sorted_json)
     assert json_dict[0]["State"] == "Andhra Pradesh"
@@ -66,7 +67,7 @@ def test_givenStateCodeCSVFile_WhenSortedByStateCode_ShouldReturnSortedResult():
 
 def test_givenCensusCSVFile_WhenSortedByPopulation_ShouldReturnSortedResult():
     census_analyser = CensusAnalyser()
-    census_analyser.census_record_counter(CENSUS_CSV_FILE_PATH)
+    census_analyser.india_census_record_counter(CENSUS_CSV_FILE_PATH)
     sorted_json = census_analyser.sort_by_population()
     json_dict = json.loads(sorted_json)
     assert json_dict[0]["Population"] == 199812341
@@ -74,7 +75,7 @@ def test_givenCensusCSVFile_WhenSortedByPopulation_ShouldReturnSortedResult():
 
 def test_givenCensusCSVFile_WhenSortedByDensity_ShouldReturnSortedResult():
     census_analyser = CensusAnalyser()
-    census_analyser.census_record_counter(CENSUS_CSV_FILE_PATH)
+    census_analyser.india_census_record_counter(CENSUS_CSV_FILE_PATH)
     sorted_json = census_analyser.sort_by_density()
     json_dict = json.loads(sorted_json)
     assert json_dict[0]["DensityPerSqKm"] == 1102
@@ -82,7 +83,12 @@ def test_givenCensusCSVFile_WhenSortedByDensity_ShouldReturnSortedResult():
 
 def test_givenCensusCSVFile_WhenSortedByArea_ShouldReturnSortedResult():
     census_analyser = CensusAnalyser()
-    census_analyser.census_record_counter(CENSUS_CSV_FILE_PATH)
+    census_analyser.india_census_record_counter(CENSUS_CSV_FILE_PATH)
     sorted_json = census_analyser.sort_by_area()
     json_dict = json.loads(sorted_json)
     assert json_dict[0]["AreaInSqKm"] == 342239
+
+
+def test_givenUSCensusCSVFile_WhenLoaded_ShouldReturnRecordCount():
+    census_analyser = CensusAnalyser()
+    assert census_analyser.us_census_record_counter(US_CENSUS_CSV_FILE_PATH) == 51
